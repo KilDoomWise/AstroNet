ae684675b244d213868b7f2acf2ee7cd8ce6c8b5
KilDoomWise <hinjamedia@gmail.com>
2024-01-07 00:17:19 +0300

    okey, spizdil kod koe otkuda

--- installer/boot.lua
+++ installer/boot.lua
@@ -1,7 +1,9 @@
 local term = require("term")
-local component = require("component")
-local gpu = component.gpu
+local gpu = require("component").gpu
 local colors = require("colors")
 
-gpu.setBackground(colors.blue)
-print("ебой?")
+
+gpu.setForeground(0x20AFFF)
+gpu.setBackground(0x20AFFF)
+gpu.set(1, 1, "AstroNet Boot Loader [!]")
+return select(2, gpu.get(1, 1)) == 0x20AFFF
