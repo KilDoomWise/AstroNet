3fe2e4d2baa709b40383cd15ae98adb426923e7d
KilDoomWise <hinjamedia@gmail.com>
2024-01-07 00:42:57 +0300

    ебой

--- installer/boot.lua
+++ installer/boot.lua
@@ -1,9 +1,9 @@
 local term = require("term")
 local gpu = require("component").gpu
 local colors = require("colors")
+local term = require("term")
 
-
+term.clear()
 gpu.setForeground(0x20AFFF)
-gpu.setBackground(0x20AFFF)
-gpu.set(1, 1, "AstroNet Boot Loader [!]")
-return select(2, gpu.get(1, 1)) == 0x20AFFF
+gpu.set(1, 1, "[BOOT] AstroNet Boot Loader")
+gpu.set(10, 1, "[BOOT] AstroNet loading config...")
